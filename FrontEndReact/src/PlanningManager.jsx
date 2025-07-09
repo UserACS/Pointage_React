74b8286041ce7b14e3c632148cfa0d6cbeca9a89
Ndeyea221 <132688281+Ndeyea221@users.noreply.github.com>
2025-07-09 01:58:03 +0000

    Implémentation du gestionnaire central des plannings

---
+++ FrontEndReact/src/PlanningManager.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PlanningsNavigation from './PlanningsNavigation';
+
+const PlanningManager = () => {
+  return <PlanningsNavigation />;
+};
+
+export default PlanningManager;
